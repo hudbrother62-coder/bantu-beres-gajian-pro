46fef44babc9e55d1e62b6b57fc673dcfe95eb20
hudbrother62-coder <hudbrother62@gmail.com>
2026-09-17 11:27:50 +0700

    feat: enable global action feedback

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { GlobalActionFeedback } from './action-feedback'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <GlobalActionFeedback />
   </StrictMode>,
 )
